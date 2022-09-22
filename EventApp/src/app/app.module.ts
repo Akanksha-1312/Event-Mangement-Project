cad0558422ca9b7da191add05d89bd010a438bc3
Akanksha <akankshaom131999@gmail.com>
2022-09-22 23:52:27 +0530

    ch

--- EventApp/src/app/app.module.ts
+++ EventApp/src/app/app.module.ts
@@ -1,24 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
+import { Routes, RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmployeeComponent } from './employee/employee.component';
 import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './home/home.component';
+
+
+const appRoutes: Routes = [
+  { path: 'employee', component: EmployeeComponent },
+  { path: '', component: HomeComponent },
+
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmployeeComponent
+    EmployeeComponent,
+    HomeComponent,
+   
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes),
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
